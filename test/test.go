88014752ca1ff368eda92238a4e43d92f8410c29
CODEBYGODWIN <oblasseg18@gmail.com>
2024-01-25 09:33:17 +0100

    [ADD] Search by name Function

--- test/test.go
+++ test/test.go
@@ -21,6 +21,8 @@ func main() {
 	fmt.Println(groups[0])
 
 	var level int
+	var name string
+
 	fmt.Printf("entrez l'ID : ")
 
 	_, err = fmt.Scan(&level)
@@ -36,5 +38,24 @@ func main() {
 		return
 	}
 	fmt.Println(group)
+
+	fmt.Print("Entrez une partie ou le nom complet du groupe: ")
+	_, err = fmt.Scan(&name)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	groups2 := Groupie.GetGroupsByName(groups, name)
+
+	if len(groups2) == 0 {
+		fmt.Printf("Aucun groupe correspondant au nom '%s' n'a été trouvé.\n", name)
+	} else {
+		fmt.Printf("Groupes correspondants:\n")
+		for _, g := range groups2 {
+			fmt.Printf("%+v\n", g)
+		}
+	}
 
 }
