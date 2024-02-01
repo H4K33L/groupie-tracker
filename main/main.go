061fbefd828bca9e93de8fe1cf026c776a7041ab
kelyan.danis@ynov.com <kelyan.danis@ynov.com>
2024-02-01 07:41:56 +0100

    ✨ i add html test, serveur test dans a start of unit test ✨

--- main/main.go
+++ main/main.go
@@ -2,26 +2,43 @@ package main
 
 import (
 	"fmt"
+    "html/template"
+    "net/http"
 
 	"Groupie"
 )
 
+var tmpl = template.Must(template.ParseFiles("Data/Page-Hangman.html"))
+
+var liste, err = Groupie.Begin("https://groupietrackers.herokuapp.com/api")
+
+var toSend = liste
+
 func main() {
-	information, err := Groupie.GetApi("https://groupietrackers.herokuapp.com/api")
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-	groups, err := Groupie.GetArtist(information.Artists)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	groups[1].Information, err = Groupie.GetLinkInfos(groups[1].Relation)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	fmt.Println(groups[1])
-	fmt.Println(groups[1].Information.Locations[2], groups[1].Information.Dates[2][0])
+
+    fmt.Println("server succefully up, go to http://localhost:8080")
+    http.HandleFunc("/",Send)
+    http.HandleFunc("/switch",Switch)
+    http.HandleFunc("/switchbis",SwitchBis)
+    http.ListenAndServe(":8080", nil)
+}
+
+func Send(w http.ResponseWriter, r *http.Request) {
+	tmpl.Execute(w, toSend)
+}
+
+func Switch(w http.ResponseWriter, r *http.Request) {
+	tmpl = template.Must(template.ParseFiles("Data/pagebis.html"))
+    letter := string(r.FormValue("name"))
+	fmt.Println(letter)
+	tmpl.Execute(w, toSend)
+}
+
+func SwitchBis(w http.ResponseWriter, r *http.Request) {
+	tmpl = template.Must(template.ParseFiles("Data/Page-Hangman.html"))
+	tmpl.Execute(w, toSend)
 }
